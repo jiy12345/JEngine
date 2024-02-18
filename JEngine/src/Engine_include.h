9dbaa87589190374f4e620c981f4f87de770670d
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2024-02-18 21:08:14 +0900

    feat: 엔진 코드들을 include하기 위한 헤더 파일 추가

---
+++ JEngine/src/Engine_include.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Managers/ResourceMgr.h"
+#include "Managers/EffectMgr.h"
+#include "Scene/SceneMgr.h"
+#include "Event/EventMgr.h"
+#include "Input/InputEventMgr.h"
+#include "Input/InputMgr.h"
+#include "Sound/FmodMgr.h"
+#include "Core/Engine.h"
+
+
+#include "Systems/AnimationSystem.h"
+#include "Systems/RenderSystem.h"
+#include "Systems/CameraSystem.h"
+#include "Systems/SoundSystem.h"
+#include "Systems/LightingSystem.h"
+#include "Systems/EffectSystem.h"
+#include "Systems/MovementSystem.h"
+
+#include "Tools/GUIMgr.h"
+#include "Physics/QuadTreeMgr.h"
+
+#include "Actors/Actor.h"
+#include "Actors/Character.h"
+#include "Actors/FX_BaseEffectActor.h"
+#include "World/Level.h"
+#include "World/Environment.h"
+#include "World/StaticMeshLevel.h"
+
+#include "UI/WriteMgr.h"
